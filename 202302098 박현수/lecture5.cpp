977017e64c13a2d8da2a2372fba919b9cd4527f1
parkhyunsu23 <phs041009@gmail.com>
2023-09-14 13:29:15 +0900

    year

---
+++ 202302098 박현수/lecture5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int year;
+	scanf_s("%d", &year);
+
+	int yy = (year % 4 == 0) && (year % 100 != 0)
+		&& (year % 400 == 0);
+
+	if (yy)
+		printf("¿±≥‚¿‘¥œ¥Ÿ.\n");
+	else
+		printf("¿±≥‚¿Ã æ∆¥’¥œ¥Ÿ.");
+	return 0;
+
+}
